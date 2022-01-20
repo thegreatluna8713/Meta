4e8838f46a5bf40a07284932d7c662757b85c8d7
thegreatluna8713 <97713123+thegreatluna8713@users.noreply.github.com>
2022-01-20 09:26:11 -0600

    Add files via upload

---
+++ 1d7e46b3-bfce-42bf-8d3c-cb9acebbf4ca/src/item.ts
@@ -0,0 +1,64 @@
+export type Props = {
+  url: string
+  name?: string
+  bnw: boolean
+}
+
+let siteName = 'facebook'
+let siteURL = 'facebook.com'
+let defaulthover = 'Facebook Page'
+
+let stringsToReplace: string[] = [
+  'http://',
+  'https://',
+  'http:',
+  'https:',
+  'www.',
+  siteURL,
+  '/',
+]
+
+export default class SMedia_Link implements IScript<Props> {
+  init() {}
+
+  spawn(host: Entity, props: Props, channel: IChannel) {
+    const link = new Entity()
+    link.setParent(host)
+
+    if (props.bnw) {
+      link.addComponent(new GLTFShape('1d7e46b3-bfce-42bf-8d3c-cb9acebbf4ca/models/facebook_bnw.glb'))
+    } else {
+      link.addComponent(new GLTFShape('1d7e46b3-bfce-42bf-8d3c-cb9acebbf4ca/models/facebook.glb'))
+    }
+
+    let url = parseURL(props.url)
+
+    let locationString = props.name ? props.name : defaulthover
+
+    link.addComponent(
+      new OnPointerDown(
+        async function () {
+          openExternalURL(url)
+        },
+        {
+          button: ActionButton.PRIMARY,
+          hoverText: locationString,
+        }
+      )
+    )
+  }
+}
+
+export function parseURL(url: string) {
+  let newURL = url.trim()
+
+  for (let str of stringsToReplace) {
+    if (newURL.substr(0, str.length) == str) {
+      newURL = newURL.substring(str.length).trim()
+    }
+  }
+
+  let finalURL = 'https://www.' + siteURL + '/' + newURL
+
+  return finalURL
+}
